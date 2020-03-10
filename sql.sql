a0ea4672b635cb9d083ef77558286e952e978dd1
CakingGG <39627993+CakingGG@users.noreply.github.com>
2020-03-10 19:19:53 +0800

    Add files via upload
    sql建表

---
+++ sql.sql
@@ -0,0 +1,52 @@
+/*
+SQLyog Enterprise v12.09 (64 bit)
+MySQL - 5.7.29 : Database - miaosha
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`miaosha` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `miaosha`;
+
+/*Table structure for table `dingdan` */
+
+DROP TABLE IF EXISTS `dingdan`;
+
+CREATE TABLE `dingdan` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `uid` bigint(20) NOT NULL,
+  `sid` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8;
+
+/*Data for the table `dingdan` */
+
+/*Table structure for table `shangpin` */
+
+DROP TABLE IF EXISTS `shangpin`;
+
+CREATE TABLE `shangpin` (
+  `id` bigint(10) NOT NULL AUTO_INCREMENT COMMENT '商品主键',
+  `name` varchar(50) CHARACTER SET latin1 NOT NULL COMMENT '商品名字',
+  `num` int(10) NOT NULL COMMENT '商品数量',
+  `flag` int(10) NOT NULL COMMENT '标志位',
+  `vision` int(10) NOT NULL COMMENT '版本号',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `shangpin` */
+
+insert  into `shangpin`(`id`,`name`,`num`,`flag`,`vision`) values (1,'iphone',100,1,0);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
